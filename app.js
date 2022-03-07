dc44ea6acc948fa3af0016a0cf7dea207b56d959
ryan <ryan.casler12@gmail.com>
2022-03-06 22:56:36 -0600

    even better

--- app.js
+++ app.js
@@ -8,26 +8,30 @@ const client = new Client({
     partials: ['CHANNEL', 'MESSAGE'],
 });
 
-
-
 client.on('messageCreate', async (msg) => {
     if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return; // do nothing if prefix isn't used
+    if (!msg.content.startsWith(prefix)) return; 
 
     const userInput = msg.content.slice(prefix.length);
     const userCmd = userInput.split(' ')[0]
     const userArg = userInput.split(' ')[1]
+    const address = userInput.split(' ')[2]
 
     console.log('message:' + msg.content + 'cmd:' + userCmd + ', args:' + userArg)
 
-    if (userCmd === commands.last5 && userArg.length > 0) {
-        msg.reply("Loading last 5 ME purchases for wallet " + userArg);
-        const reply = await getLastMsgs(userArg);
-        // console.log(reply)
+    var useAddress = userArg == commands.address
 
+    if (userCmd === commands.whale && useAddress && userArg.length > 0) {
+        // Keepig this here, this is how to just reply in the channel to the person directly
+        // msg.reply("Loading last 5 ME purchases for wallet " + userArg);
+        const reply = await getLastMsgs(address);
+        msg.channel.send({ embeds: reply })
+    } else if (userCmd == commands.whale && userArg in whales) {
+        var addy = adys.userArg
+        const reply = await getLastMsgs(addy);
         msg.channel.send({ embeds: reply })
     } else {
-        msg.reply('I do not understand your command');
+        msg.reply('LOL try again');
     }
 
     //   else if (userCmd === commands.lastMsgs) {
@@ -37,9 +41,7 @@ client.on('messageCreate', async (msg) => {
 });
 
 const getLastMsgs = async (wallet) => {
-    // fetching the last 10 messages
-    //   const res = await msg.channel.messages.fetch({ limit: 10 });
-    let url = "https://api-mainnet.magiceden.dev/v2/wallets/" + wallet + "/activities?offset=0&limit=100"
+    let url = `https://api-mainnet.magiceden.dev/v2/wallets/${wallet}/activities?offset=0&limit=100`
    
     let getRequest = fetch(url, {
         method: 'GET',
@@ -50,20 +52,16 @@ const getLastMsgs = async (wallet) => {
 
         let meResponse = await getRequest
 
-     let last5 = meResponse.filter(action => action.buyer == wallet && (action.type == 'buy' || action.type == 'buyNow'))
-        .slice(0, 5);
-        
-    //   const lastTenMsgs = res.map((message) => {
-    //     return message.content;
-    //   });
+     let last15 = meResponse.filter(action => action.buyer == wallet && (action.type == 'buy' || action.type == 'buyNow'))
+        .slice(0, 15);
 
-            console.log(last5)
+    console.log(last15)
 
       const embeds = [];
 
-      last5.forEach((purchase, index) => {
+      last15.forEach((purchase, index) => {
         const embed = new MessageEmbed()
-          .setColor('#00FFA3') // can be hex like #3caf50
+          .setColor('#00FFA3')
           .setTitle(purchase.collection)
           .setDescription(`${purchase.price} SOL`);
 
@@ -76,10 +74,6 @@ const getLastMsgs = async (wallet) => {
 
 client.on('ready', () => {
     console.log('Logged in as ' + client.user.tag + "\n\n\n");
-    //   const Guilds = client.guilds.cache.map(guild => guild.id);
-    //     Guilds.forEach(element => {
-    //         element.channels.get('general').send('testing bro')
-    //       });
 });
 
 const startBot = () => {
--- config/config.js
+++ config/config.js
@@ -5,20 +5,26 @@ const { DIRECT_MESSAGES, GUILD_MESSAGES, GUILDS } = Intents.FLAGS;
 const botIntents = [DIRECT_MESSAGES, GUILD_MESSAGES, GUILDS];
 
 const commands = {
-  last5: 'last5',
+  whale: 'whale',
+  address: 'address',
+};
+
+const whales = {
   cozy: 'cozy',
   knox: 'knox',
   bigBrain: 'bigbrain',
-  solHub: 'solHub'
+  solHub: 'solHub',
+  ravers: 'ravers',
 };
 
 const addys = {
   cozy: 'EAHJNfFDtivTMzKMNXzwAF9RTAeTd4aEYVwLjCiQWY1E',
   knox: 'DxKc73eJX5J1kY5ND69hnLs7ox64Q2exN3BVUWxBtBjo',
   bigBrain: '7iaZ47A6FmvCXJysMAaJYRNkVutksYBZEANFGSpDYhpN',
-  solHub: 'DQ977NcueXqkDwTKjP5nsgmSgmsqstRo2XxGK2aHHKPr'
+  solHub: 'DQ977NcueXqkDwTKjP5nsgmSgmsqstRo2XxGK2aHHKPr',
+  ravers: 'GozLuKr4uvXM5k4z5KrMdQbBBkRMh3sVBgDPXtrWd7xX'
 };
 
 const prefix = '!';
 
-module.exports = { botIntents, prefix, commands, addys };
+module.exports = { botIntents, prefix, commands, addys, whales };
